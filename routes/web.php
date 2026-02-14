b5c8f66334643397fac5c59b87d7e0c17ec6cb79
Jennefer Dahug <dahug_jennefer@plpasig.edu.ph>
2026-02-14 17:14:57 +0800

    missing something

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Services\UserService;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello, World!";
+});
+
+Route::get('/show-users', [UserController::class, 'show']);
+
+// Service container
+Route::get('/test-container', function (Request $request) {
+    $input = $request->input('key');
+    return $input;
+});
+
+// Service provider
+Route::get('/test-provider', function (UserService $userService) {
+    return $userService->listUsers();
 });
